b3b3bdb580663f12ff804fd098fdd46807d85730
zakariamelk <melkbox@gmail.com>
2024-04-20 18:08:41 +0100

    Add OrderEnum unit tests

--- domain/src/main/java/com/moweritnow/model/InvalidOrderException.java
+++ domain/src/main/java/com/moweritnow/model/InvalidOrderException.java
@@ -1,5 +1,8 @@
 package com.moweritnow.model;
 
+import lombok.Getter;
+
+@Getter
 public class InvalidOrderException extends Exception {
     private final char order;
 
---
+++ domain/src/test/java/com/moweritnow/model/OrderEnumTest.java
@@ -0,0 +1,32 @@
+package com.moweritnow.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderEnumTest {
+
+    @Test
+    public void testFromChar_valid() throws InvalidOrderException {
+        // Given
+        char c = 'A';
+        // When
+        OrderEnum orderEnum = OrderEnum.fromChar(c);
+        // Then
+        assertEquals(OrderEnum.A,orderEnum);
+    }
+
+    @Test
+    public void testFromChar_notValid() {
+        // Given
+        char c = 'E';
+        // When
+        Exception exception = assertThrows(InvalidOrderException.class,()->{
+            OrderEnum.fromChar(c);
+        });
+        // Then
+        assertInstanceOf(InvalidOrderException.class,exception);
+        assertEquals(c,((InvalidOrderException)exception).getOrder());
+    }
+
+}
